68e9fbce9d1fab77eb7abb12e0976d50f9e2384a
Desecho <desecho@gmail.com>
2013-03-18 02:50:18 +0400

    minor code fix

--- movies_project/movies/models.py
+++ movies_project/movies/models.py
@@ -4,13 +4,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.conf import settings
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-    settings = JSONField(null=True)
-
-    def __unicode__(self):
-        return self.user.username
-
 
 class List(models.Model):
     title = models.CharField(max_length=255)
