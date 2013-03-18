0313af79a24d347640bb211e304bc7a34a63c82c
Desecho <desecho@gmail.com>
2013-03-18 18:46:38 +0400

    some translation and addition of imdb rating update script

---
+++ movies_project/movies/management/commands/update_imdb_ratings.py
@@ -0,0 +1,17 @@
+import urllib2
+import json
+from django.core.management.base import BaseCommand
+from movies.models import Movie
+
+class Command(BaseCommand):
+    help = 'Updates the IMDB ratings'
+    def handle(self, *args, **options):
+        movies = Movie.objects.all()
+        for movie in movies:
+            html = urllib2.urlopen("http://www.imdbapi.com/?i=%s" % movie.imdb_id).read()
+            imdb_data = json.loads(html)
+            rating = imdb_data.get('imdbRating')
+            if rating == 'N/A':
+                rating = None
+            movie.imdb_rating = rating
+            movie.save()
--- movies_project/movies/models.py
+++ movies_project/movies/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf8 -*-
 from django.db import models
 from annoying.fields import JSONField
 from django.contrib.auth.models import User
@@ -6,27 +7,35 @@ from django.conf import settings
 
 
 class List(models.Model):
-    title = models.CharField(max_length=255)
-    key_name = models.CharField(max_length=255)
+    title = models.CharField('название', max_length=255)
+    key_name = models.CharField('ключевое имя', max_length=255)
+
+    class Meta:
+        verbose_name = 'список'
+        verbose_name_plural = 'списки'
 
     def __unicode__(self):
         return self.title
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255)
-    plot = models.TextField(null=True)
-    director = models.CharField(max_length=255, null=True)
-    writer = models.CharField(max_length=255, null=True)
-    genre = models.CharField(max_length=255, null=True)
-    actors = models.CharField(max_length=255, null=True)
-    imdb_id = models.CharField(max_length=15, unique=True)
-    tmdb_id = models.IntegerField(unique=True)
-    imdb_rating = models.DecimalField(max_digits=2, decimal_places=1, null=True)
-    poster = models.URLField(max_length=255, null=True)
-    release_date = models.DateField(null=True)
-    homepage = models.URLField(null=True)
-    trailers = JSONField(null=True)
+    title = models.CharField('название', max_length=255)
+    plot = models.TextField('описание', null=True)
+    director = models.CharField('режиссёр', max_length=255, null=True)
+    writer = models.CharField('сценарист', max_length=255, null=True)
+    genre = models.CharField('жанр', max_length=255, null=True)
+    actors = models.CharField('актёры', max_length=255, null=True)
+    imdb_id = models.CharField('IMDB id', max_length=15, unique=True)
+    tmdb_id = models.IntegerField('TMDB id', unique=True)
+    imdb_rating = models.DecimalField('IMDB рейтинг', max_digits=2, decimal_places=1, null=True)
+    poster = models.URLField('постер', max_length=255, null=True)
+    release_date = models.DateField('дата выпуска', null=True)
+    homepage = models.URLField('сайт', null=True)
+    trailers = JSONField('трейлеры', null=True)
+
+    class Meta:
+        verbose_name = 'фильм'
+        verbose_name_plural = 'фильмы'
 
     def __unicode__(self):
         return self.title
@@ -49,11 +58,15 @@ class Movie(models.Model):
 
 
 class Record(models.Model):
-    user = models.ForeignKey(User)
-    movie = models.ForeignKey(Movie)
-    list = models.ForeignKey(List)
-    rating = models.IntegerField(default=0)
-    comment = models.CharField(max_length=255, default='')
+    user = models.ForeignKey(User, verbose_name='пользователь')
+    movie = models.ForeignKey(Movie, verbose_name='фильм')
+    list = models.ForeignKey(List, verbose_name='список')
+    rating = models.IntegerField('рейтинг', default=0)
+    comment = models.CharField('комментарий', max_length=255, default='')
+
+    class Meta:
+        verbose_name = 'запись'
+        verbose_name_plural = 'записи'
 
     def __unicode__(self):
         return self.movie.title
