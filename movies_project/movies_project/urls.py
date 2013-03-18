36d52057ad1296f183367a4f97a59d80e061d964
Desecho <desecho@gmail.com>
2013-03-18 15:19:31 +0400

    minor fixes

--- movies_project/movies_project/urls.py
+++ movies_project/movies_project/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'movies.views.search'),
@@ -23,8 +23,8 @@ urlpatterns = patterns('',
     url(r'^logout/$', 'movies.views.logout_view'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
