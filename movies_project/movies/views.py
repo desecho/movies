65e71da1efdf4fc81f6cb7bdf7f2f24967408109
Desecho <desecho@gmail.com>
2013-03-26 13:31:31 +0400

    fixed search bug from last commit

--- movies_project/movies/views.py
+++ movies_project/movies/views.py
@@ -13,7 +13,8 @@ from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.views.decorators.csrf import ensure_csrf_cookie
 from movies.functions import (get_friends, filter_movies_for_recommendation,
-                              add_movie_to_list, add_to_list_from_db)
+                              add_movie_to_list, add_to_list_from_db,
+                              get_movies_from_tmdb)
 
 import logging
 logger = logging.getLogger('movies.test')
@@ -180,114 +181,6 @@ def ajax_download(request):
 
 @ajax_request
 def ajax_search_movie(request):
-    def get_movies_from_tmdb(query, type, options):
-        def set_proper_date(movies):
-            def format_date(date):
-                if date:
-                    return date.strftime('%d.%m.%y')
-            m = []
-            for movie in movies:
-                movie['release_date'] = format_date(movie['release_date'])
-                m.append(movie)
-            return movies
-
-        # def sortByPopularity(movies):
-        #     movies = sorted(movies, key=itemgetter('popularity'), reverse=True)
-        #     for movie in movies:
-        #         del movie['popularity']
-        #     return movies
-        def remove_not_popular_movies(movies):
-            m = []
-            for movie in movies:
-                if movie['popularity'] > settings.MIN_POPULARITY:
-                    del movie['popularity']
-                    m.append(movie)
-            if not m:                  # keep initial movie list if all are unpopular
-                m = movies
-            return m
-
-        def sort_by_date(movies):
-            movies_with_date = []
-            movies_without_date = []
-            for movie in movies:
-                if movie['release_date']:
-                    movies_with_date.append(movie)
-                else:
-                    movies_without_date.append(movie)
-            if not movies_with_date and not movies_without_date:      # These two lines - to check if no movies are left because of low popularity
-                movies_with_date = movies                             # ---
-            movies_with_date = sorted(movies_with_date, key=itemgetter('release_date'), reverse=True)
-            movies = movies_with_date + movies_without_date
-            return movies
-
-        def get_posterUrl(poster):
-            if poster:
-                url = settings.POSTER_BASE_URL + settings.POSTER_SIZE_SMALL + '/' + poster.filename
-            else:
-                url = settings.NO_POSTER_IMAGE_URL
-            return url
-
-        def get_data(query, type):
-            query = query.encode('utf-8')
-            '''Types - 1 - movie, 2 - actor, 3 - director
-               for actor, director search - the first is used.'''
-            if type == 1:
-                try:
-                    movies = tmdb3.searchMovie(query)
-                except:
-                    return -1
-            else:
-                try:
-                    result = tmdb3.searchPerson(query)
-                except:
-                    return -1
-                movies = []
-                if len(result):
-                    person = result[0]
-                    if type == 2:
-                        movies = person.roles
-                    else:
-                        for m in person.crew:
-                            if m.job == 'Director':
-                                movies.append(m)
-            return movies
-
-        output = {}
-
-        results = get_data(query, type)
-        if results == -1:
-            output['status'] = -1
-            return output
-        movies = []
-        i = 0
-        if len(results):
-            try:
-                for result in results:
-                    i += 1
-                    if i > settings.MAX_RESULTS:
-                        break
-                    movie = {
-                        'id': result.id,
-                        'release_date': result.releasedate,
-                        'popularity': result.popularity,                    # for popularity sorting
-                        'title': result.originaltitle,
-                        'poster': get_posterUrl(result.poster),
-                    }
-                    movies.append(movie)
-            except IndexError:                                              # strange exception in 'matrix case'
-                pass
-            if options['popular_only']:
-                movies = remove_not_popular_movies(movies)
-            if options['sort_by_date']:
-                movies = sort_by_date(movies)
-            #movies = sortByPopularity(movies)
-
-            movies = set_proper_date(movies)
-            output['status'] = 1
-            output['movies'] = movies
-        else:
-            output['status'] = 0
-        return output
     if request.is_ajax() and request.method == 'POST':
         POST = request.POST
         if 'query' in POST and 'type' in POST and 'options[popular_only]' in POST and 'options[sort_by_date]' in POST:
