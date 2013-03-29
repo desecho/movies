1afdf3ad10d36084c51517d45c352c04357ca2ca
Desecho <desecho@gmail.com>
2013-03-29 05:01:18 +0400

    added administration for actions, fixed user unicode, fixed unicode of actions

--- movies_project/movies/admin.py
+++ movies_project/movies/admin.py
@@ -1,10 +1,17 @@
-from movies.models import Movie, Record, List
+from movies.models import Movie, Record, List, Action, ActionRecord
 from django.contrib import admin
 
 
 class RecordAdmin(admin.ModelAdmin):
     list_display = ['user', 'movie', 'date']
 
+
+class ActionRecordAdmin(admin.ModelAdmin):
+    list_display = ['user', 'movie', 'action', 'date']
+
+
 admin.site.register(Movie)
 admin.site.register(Record, RecordAdmin)
 admin.site.register(List)
+admin.site.register(Action)
+admin.site.register(ActionRecord, ActionRecordAdmin)
