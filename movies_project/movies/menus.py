6401ac930fbc1eaabde8f07b07ec5f98b88f29bc
Desecho <desecho@gmail.com>
2013-03-30 19:04:44 +0400

    feed changes, added feed for people

--- movies_project/movies/menus.py
+++ movies_project/movies/menus.py
@@ -2,33 +2,36 @@
 from menu import Menu, MenuItem
 from django.core.urlresolvers import reverse
 
-Menu.add_item('main', MenuItem(
-    'Просмотрено',
+Menu.add_item('main', MenuItem('Просмотрено',
     reverse('movies.views.list', kwargs=({'list': 'watched'})),
     weight=10,))
 
-Menu.add_item('main', MenuItem(
-    'К просмотру',
+Menu.add_item('main', MenuItem('К просмотру',
     reverse('movies.views.list', kwargs=({'list': 'to-watch'})),
     weight=10,))
 
-Menu.add_item('main', MenuItem(
-    'Рекомендации',
+Menu.add_item('main', MenuItem('Рекомендации',
     reverse('movies.views.recommendation'),
     weight=10,))
 
-Menu.add_item('main', MenuItem(
-    'Друзья',
+Menu.add_item('main', MenuItem('Друзья',
     reverse('movies.views.friends'),
-    weight=20,
-    separator=True,))
-
-Menu.add_item('main', MenuItem(
-    'Люди',
-    reverse('movies.views.people'),
     weight=20,))
 
-Menu.add_item('main', MenuItem(
-    'Лента',
-    reverse('movies.views.feed'),
+feed_children = (
+    MenuItem('Друзья',
+        reverse('movies.views.feed', kwargs=({'list': 'friends'})),
+        weight=20,),
+    MenuItem('Люди',
+        reverse('movies.views.feed', kwargs=({'list': 'people'})),
+        weight=20,),
+)
+
+Menu.add_item('main', MenuItem('Лента',
+    reverse('movies.views.search'),  # doesn't really matter because of the submenus
+    weight=20,
+    children=feed_children))
+
+Menu.add_item('main', MenuItem('Люди',
+    reverse('movies.views.people'),
     weight=20,))
