5d0e0c3d5a95049752388ba5cfc5adffe84c758d
Desecho <desecho@gmail.com>
2013-04-02 04:21:15 +0400

    settings fixed

--- movies_project/movies_project/settings.py
+++ movies_project/movies_project/settings.py
@@ -95,6 +95,7 @@ try:
     from secret import SECRET_KEY
 except ImportError:
     def gen_secret_key():
+        here = lambda x: os.path.abspath(os.path.join(os.path.dirname(__file__), x))
         print "Django's SECRET_KEY not found, generating new."
         from random import choice
         secret_key = ''.join([choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
