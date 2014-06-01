554fe769f20e029e95641e408e52b9a811448a5e
Desecho <desecho@gmail.com>
2014-06-01 06:16:03 +0400

    added command

---
+++ movies_project/movies/management/commands/get_wrong_russian_titles.py
@@ -0,0 +1,15 @@
+# -*- coding: utf8 -*-
+from movies.models import Movie
+from django.core.management.base import BaseCommand
+
+ALLOWED_CHARS = u'абвгдеёжзийклмнопрстуфхцчшщьыъэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ123456789 -:0,"«».?+/'
+
+class Command(BaseCommand):
+    help = 'Displays wrong russian titles'
+
+    def handle(self, *args, **options):
+        for movie in Movie.objects.all():
+            for letter in movie.title_ru:
+                if letter not in ALLOWED_CHARS:
+                    print '%d - %s - %s' % (movie.id, movie.title_ru, movie.title)
+                    break
