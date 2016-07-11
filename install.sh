82855b1744e8c3fd923f1bd82ae4c244c8cbedf1
Anton Samarchyan <desecho@gmail.com>
2016-07-11 04:03:53 -0400

    fixes

--- install.sh
+++ install.sh
@@ -14,7 +14,9 @@ mkdir logs
 
 #install crontab records
 crontab -l > crontab
-echo "0 0 * * * $PYTHON_PATH/python $PWD/src/manage.py update_vk_profiles > $PWD/logs/update_vk_profiles.log 2>&1" > crontab
-echo "0 0 1 * * $PYTHON_PATH/python $PWD/scripts/update_movie_data.sh > $PWD/logs/update_movie_data.log 2>&1" > crontab
+echo "0 0 * * * $PYTHON_PATH/python $PWD/src/manage.py update_vk_profiles > $PWD/logs/update_vk_profiles.log 2>&1" >> crontab
+echo "0 0 1 * * $PYTHON_PATH/python $PWD/scripts/update_movie_data.sh > $PWD/logs/update_movie_data.log 2>&1" >> crontab
 crontab crontab
 rm crontab
+
+$PYTHON_PATH/python src/manage.py collectstatic
