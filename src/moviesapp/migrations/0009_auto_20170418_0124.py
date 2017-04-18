02e3e47a9a01908b38e3e0f34e150f13e990f338
Anton Samarchyan <desecho@gmail.com>
2017-04-18 01:49:10 -0400

    fix translations for the feeds page

---
+++ src/moviesapp/migrations/0009_auto_20170418_0124.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('moviesapp', '0008_user_loaded_initial_data'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='action',
+            name='name_en',
+            field=models.CharField(max_length=255, null=True),
+        ),
+        migrations.AddField(
+            model_name='action',
+            name='name_ru',
+            field=models.CharField(max_length=255, null=True),
+        ),
+    ]
---
+++ src/moviesapp/migrations/0010_auto_20170418_0137.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('moviesapp', '0009_auto_20170418_0124'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='list',
+            name='name_en',
+            field=models.CharField(max_length=255, null=True),
+        ),
+        migrations.AddField(
+            model_name='list',
+            name='name_ru',
+            field=models.CharField(max_length=255, null=True),
+        ),
+    ]
