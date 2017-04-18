13d7339f4a434aa5d76edb2580c4efc3695da98e
Anton Samarchyan <desecho@gmail.com>
2017-04-18 03:03:56 -0400

    changes to get_wrong_russian_titles command

--- src/moviesapp/management/commands/get_wrong_russian_titles.py
+++ src/moviesapp/management/commands/get_wrong_russian_titles.py
@@ -1,18 +1,24 @@
 # coding: utf-8
+
+from tabulate import tabulate
+
 from django.core.management.base import BaseCommand
 
 from ...models import Movie
 
-ALLOWED_CHARS = u'абвгдеёжзийклмнопрстуфхцчшщьыъэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ123456789 -:0,"«».?+/–º·№!'
-
+ALLOWED_CHARS = u'абвгдеёжзийклмнопрстуфхцчшщьыъэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ123456789 -:0,"«».?+/–º·№!()½⅓\''
 
 class Command(BaseCommand):
     help = 'Displays wrong russian titles'
 
     def handle(self, *args, **options):
-        for movie in Movie.objects.all():
+        ids_to_ignore = [1989, 1873, 1756, 1569, 1296, 931, 845, 376, 406]
+        rows = []
+        for movie in Movie.objects.exclude(pk__in=ids_to_ignore):
             for letter in movie.title_ru:
                 if letter not in ALLOWED_CHARS:
-                    print '%d - %s - %s' % (movie.id, movie.title_ru,
-                                            movie.title_original)
+                    rows.append((movie.id, movie.title_ru, movie.title_original))
                     break
+        headers = ('Id', 'Title (ru)', 'Title (eng)')
+        print tabulate(rows, headers, 'fancy_grid')
+        print 'Total: %d' % len(rows)
