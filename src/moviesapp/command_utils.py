4f5558dd262df264457a97816acf401acb97f4ce
Anton Samarchyan <desecho@gmail.com>
2017-04-19 01:21:49 -0400

    refactoring

--- src/moviesapp/command_utils.py
+++ src/moviesapp/command_utils.py
@@ -1,3 +1,6 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
 import sys
 
 from tqdm import tqdm as tqdm_original
--- src/moviesapp/social.py
+++ src/moviesapp/social.py
@@ -1,3 +1,6 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
 import facebook
 import vkontakte
 from django.conf import settings
