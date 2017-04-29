efb9391301696e646161fd0a8dbb702417874707
Anton Samarchyan <desecho@gmail.com>
2017-04-29 06:12:28 -0400

    get_wrong_russian_titles command changes

--- src/moviesapp/management/commands/get_wrong_russian_titles.py
+++ src/moviesapp/management/commands/get_wrong_russian_titles.py
@@ -13,13 +13,16 @@ class Command(BaseCommand):
     help = 'Displays wrong russian titles'
 
     def handle(self, *args, **options):
-        ids_to_ignore = [1989, 1873, 1756, 1569, 1296, 931, 845, 376, 406]
+        ids_to_ignore = [
+            1989, 1873, 1756, 1569, 1296, 931, 845, 376, 406, 450, 1872, 1622, 1643, 1614, 1597,
+            471, 786, 1153, 1041, 653,
+        ]
         rows = []
         for movie in Movie.objects.exclude(pk__in=ids_to_ignore):
             for letter in movie.title_ru:
                 if letter not in ALLOWED_CHARS:
-                    rows.append((movie.id, movie.title_ru, movie.title_original))
+                    rows.append((movie.id, movie.tmdb_id, movie.title_ru, movie.title_original))
                     break
-        headers = ('Id', 'Title (ru)', 'Title (eng)')
+        headers = ('Id', 'TMDB Id', 'Title (ru)', 'Title (eng)')
         self.info(tabulate(rows, headers, 'fancy_grid'))
         self.info('Total: %d' % len(rows))
