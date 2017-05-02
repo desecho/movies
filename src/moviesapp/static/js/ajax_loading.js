047edc815ce35a4874a3fe8eb9d9fe1e9a1de3e7
Anton Samarchyan <desecho@gmail.com>
2017-05-02 17:44:20 -0400

    Removed unnecessary code

--- src/moviesapp/static/js/ajax_loading.js
+++ src/moviesapp/static/js/ajax_loading.js
@@ -1,21 +1,4 @@
 $(function(){
-  const spinnerOptions = {
-    lines: 12,
-    length: 5,
-    width: 3,
-    radius: 6,
-    corners: 1,
-    rotate: 40,
-    color: '#000',
-    speed: 1,
-    trail: 60,
-    shadow: false,
-    hwaccel: true,
-    className: 'spinner',
-    zIndex: 2e9,
-    top: 'auto',
-    left: 'auto'
-  }
   $(document).ajaxStart(function(){
     mprogress.start();
   });
