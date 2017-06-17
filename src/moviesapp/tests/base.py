3ac3c5e27e67f70b59c33ef05005392de5c757c4
Anton Samarchyan <desecho@gmail.com>
2017-06-17 00:55:54 -0400

    Minor changes in test

--- src/moviesapp/tests/base.py
+++ src/moviesapp/tests/base.py
@@ -16,23 +16,29 @@ class BaseTestCase(TestCase):
         'users.json',
     ]
 
+    USER_USERNAME = 'neo'
     USER_PWD = 'password'
     # Superuser - admin/adminpassword
     # Another user - fox/password
 
     @staticmethod
+    def get_content(response):
+        return response.content.decode('utf-8')
+
+    @staticmethod
     def get_soup(response):
         return BeautifulSoup(response.content)
 
-    @staticmethod
-    def get_json(response):
-        return json.loads(response.content.decode('utf-8'))
+    def get_json(self, response):
+        return json.loads(self.get_content(response))
 
     def setUp(self):
         User = get_user_model()
-        self.user = User.objects.get(username='neo')
+        self.user = User.objects.get(username=self.USER_USERNAME)
 
-    def login(self, username='neo'):
+    def login(self, username=None):
+        if username is None:
+            username = self.USER_USERNAME
         self.client.logout()
         self.client.login(
             username=username,
