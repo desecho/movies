a6380958ac2bb3de057721e288502a333f599d71
Anton Samarchyan <desecho@gmail.com>
2018-02-06 03:16:27 -0500

    Change counter html

--- src/moviesapp/templatetags/movie_count.py
+++ src/moviesapp/templatetags/movie_count.py
@@ -15,4 +15,4 @@ def movie_count(user):
 
     watched = create_span_tag(_('Watched'), LIST_IDS['watched'])
     to_watch = create_span_tag(_('To Watch'), LIST_IDS['to_watch'])
-    return mark_safe(f'{watched} / {to_watch}')  # nosec
+    return mark_safe(f'<i class="fa fa-eye"></i> {watched} &nbsp;<i class="fa fa-eye-slash"></i> {to_watch}')  # nosec
