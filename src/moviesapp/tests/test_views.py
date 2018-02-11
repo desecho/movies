6e9c3c0924dce6b0890a446ad21b4263945d6de0
Anton Samarchyan <desecho@gmail.com>
2018-02-11 17:47:23 -0500

    Add test

---
+++ src/moviesapp/tests/test_views.py
@@ -0,0 +1,10 @@
+from django.urls import reverse
+
+from .base import BaseTestCase
+
+
+class AboutTestCase(BaseTestCase):
+    def test_about(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
