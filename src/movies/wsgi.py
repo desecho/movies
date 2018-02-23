a23d80efa4711923f8d113f8b5d43c483eb28420
Anton Samarchyan <desecho@gmail.com>
2018-02-22 23:45:59 -0500

    Improve sentry support

--- src/movies/wsgi.py
+++ src/movies/wsgi.py
@@ -7,7 +7,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 import os
 
 from django.core.wsgi import get_wsgi_application
+from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "movies.settings")
 
-application = get_wsgi_application()
+application = Sentry(get_wsgi_application())
