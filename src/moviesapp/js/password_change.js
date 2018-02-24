0c2589e121bdb7e007c4c4e8236708b26189fb02
Anton Samarchyan <desecho@gmail.com>
2018-02-24 02:08:25 -0500

    Bugfix - password change not working

--- src/moviesapp/js/password_change.js
+++ src/moviesapp/js/password_change.js
@@ -2,7 +2,7 @@
 
 $('#password-change').submit(function(e) {
   e.preventDefault();
-  const password = $('#new_password1')[0].value;
-  $('#new_password2').value = password;
+  const password = $('#new_password1').val();
+  $('#new_password2').val(password)
   $('#password-change')[0].submit();
 });
