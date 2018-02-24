3a2ce0ef57eaa58d5c08760c7372009bb77a94a0
Anton Samarchyan <desecho@gmail.com>
2018-02-24 02:36:42 -0500

    Fix down arrow in select input elements for firefox on linux

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ module.exports = {
     ],
   },
   plugins: [
-    new ExtractTextPlugin('css/[name].css'),
+    new ExtractTextPlugin('[name].css'),
     new webpack.ProvidePlugin({
       '$': 'jquery',
       'jQuery': 'jquery',
