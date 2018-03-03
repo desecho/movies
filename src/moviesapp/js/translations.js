ffdb7df2cf9c60c9de4c835b2c767b5e3d6252b6
Anton Samarchyan <desecho@gmail.com>
2018-03-03 02:19:59 -0500

    Fix some js translations

---
+++ src/moviesapp/js/translations.js
@@ -0,0 +1,6 @@
+// We need this because we programatically use these translations in javascript and it does not get detected
+
+'use strict';
+
+gettext('Actor');
+gettext('Director');
---
+++ src/moviesapp/translations.py
@@ -0,0 +1,6 @@
+"""We need this because we programatically use these translations in javascript and it does not get detected."""
+
+from django.utils.translation import gettext_lazy as _
+
+_('Actor')
+_('Director')
