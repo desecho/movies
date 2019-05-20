9a6c84fa1f23590228b2c41c1ef38aadd598e478
Anton Samarchyan <desecho@gmail.com>
2019-05-20 09:49:24 -0400

    Add unify linter

--- clean.sh
+++ clean.sh
@@ -3,6 +3,7 @@
 # We want to remove imports before running isort.
 autoflake --remove-all-unused-imports --in-place -r src
 yapf -ri src
+unify -ri src
 # We want to run isort after yapf to make sure isort lint pass.
 isort -rc src
 csscomb src/moviesapp/styles/*
