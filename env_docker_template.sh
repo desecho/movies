46078dbc73b74b0fab6bfd2431fa87db27414d34
Anton Samarchyan <desecho@gmail.com>
2021-04-13 02:04:40 -0400

    Simplify env_docker_template.sh

--- env_docker_template.sh
+++ env_docker_template.sh
@@ -4,40 +4,7 @@
 export DEBUG=
 # bool
 export COLLECT_STATIC=
-# bool
-export IS_DEV=
-export SECRET_KEY=key
-export PROJECT_DOMAIN=localhost
-export ADMIN_EMAIL=
-export INTERNAL_IP=
-export RAVEN_DSN=
-export GOOGLE_ANALYTICS_ID=id
 export STATIC_URL=
-
-export DB_USER=root
-export DB_PASSWORD=password
 export DB_HOST=host.docker.internal
-
-export SOCIAL_AUTH_VK_APP_KEY=
-export SOCIAL_AUTH_VK_APP_SECRET=
-
-export SOCIAL_AUTH_VK_OAUTH2_KEY=
-export SOCIAL_AUTH_VK_OAUTH2_SECRET=
-
-export SOCIAL_AUTH_FACEBOOK_KEY=
-export SOCIAL_AUTH_FACEBOOK_SECRET=
-
-export TMDB_KEY=
-export OMDB_KEY=
-
-export HOST_MOVIES_TEST=
 # bool
 export IS_VK_DEV=
-
-# bool
-export EMAIL_USE_SSL=
-export EMAIL_HOST=
-export EMAIL_HOST_USER=
-export EMAIL_HOST_PASSWORD=
-# int
-export EMAIL_PORT=465
