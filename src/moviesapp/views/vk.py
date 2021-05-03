93824d980ce08f6ee7716645ba7f57184bfdf101
Anton Samarchyan <desecho@gmail.com>
2021-05-03 02:36:44 -0400

    Fix linting errors

--- src/moviesapp/views/vk.py
+++ src/moviesapp/views/vk.py
@@ -16,14 +16,16 @@ class UploadPosterToWallView(VkAjaxView):
         movie = Record.objects.get(pk=record_id).movie
         file_contents = requests.get(movie.poster_big).content
         # We need to make sure that file is not deleted that is why we use `self`.
-        self.tmp_file = tempfile.NamedTemporaryFile(suffix=".jpg")
+        # TODO: remove pylint warning. Need to test this properly.
+        self.tmp_file = tempfile.NamedTemporaryFile(suffix=".jpg")  # pylint: disable=consider-using-with
         self.tmp_file.write(file_contents)
         os.chmod(self.tmp_file.name, 0o666)  # nosec
 
     @staticmethod
     def _upload_file(url, filepath):
-        file_ = open(filepath, "rb")
-        data = MultipartEncoder(fields={"photo": (file_.name, file_, "image/jpg")})
+        # TODO: need to test this:
+        with open(filepath, "rb") as f:
+            data = MultipartEncoder(fields={"photo": (f.name, f, "image/jpg")})
         return requests.post(url, data=data, headers={"Content-Type": data.content_type}).text
 
     def post(self, request, **kwargs):
