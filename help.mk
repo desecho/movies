69ff3b3d780b7f3a867d45db5f8019e4aceaaff8
Anton Samarchyan <desecho@gmail.com>
2022-04-13 20:53:45 -0400

    Put Makefile help into a separate file

---
+++ help.mk
@@ -0,0 +1,32 @@
+TARGET_MAX_CHAR_NUM := 25
+
+# COLORS
+GREEN   := \033[0;32m
+YELLOW  := \033[0;33m
+RESET   := \033[0;10m
+
+.PHONY: help
+## Show help | Help
+help:
+	@echo ''
+	@echo 'Usage:'
+	@printf "  ${YELLOW}make${RESET} ${GREEN}<target>${RESET}"
+	@echo ''
+	@echo ''
+	@echo 'Targets:'
+	@awk '/^[a-zA-Z\-\_0-9]+:/ { \
+		helpMessage = match(lastLine, /^## (.*)/); \
+		if (helpMessage) { \
+		    if (index(lastLine, "|") != 0) { \
+				stage = substr(lastLine, index(lastLine, "|") + 1); \
+				printf "\n ${GRAY}%s: \n", stage;  \
+			} \
+			helpCommand = substr($$1, 0, index($$1, ":")-1); \
+			helpMessage = substr(lastLine, RSTART + 3, RLENGTH); \
+			if (index(lastLine, "|") != 0) { \
+				helpMessage = substr(helpMessage, 0, index(helpMessage, "|")-1); \
+			} \
+			printf "    ${YELLOW}%-$(TARGET_MAX_CHAR_NUM)s${RESET} ${GREEN}%s${RESET}\n", helpCommand, helpMessage; \
+		} \
+	} \
+	{ lastLine = $$0 }' $(MAKEFILE_LIST)
