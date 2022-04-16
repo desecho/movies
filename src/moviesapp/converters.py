70de81a941195c7b421fcc8dbf7d03cf811cff75
Anton Samarchyan <desecho@gmail.com>
2022-04-16 11:23:48 -0400

    Fix lint errors

--- src/moviesapp/converters.py
+++ src/moviesapp/converters.py
@@ -1,12 +1,14 @@
 class Converter:
-    def to_python(self, value):
+    def to_python(self, value):  # pylint:disable=no-self-use
         return value
 
-    def to_url(self, value):
+    def to_url(self, value):  # pylint:disable=no-self-use
         return value
+
 
 class ListConverter(Converter):
     regex = "watched|to-watch"
+
 
 class FeedConverter(Converter):
     regex = "people|friends"
