5ed332009c699490ed6c9c84c5fcfde5b66f09c0
Anton Samarchyan <desecho@gmail.com>
2022-05-07 15:26:00 -0400

    Improve load_db script

--- scripts/load_db.sh
+++ scripts/load_db.sh
@@ -3,6 +3,6 @@
 set -eou pipefail
 
 YESTERDAY=$(date -d "yesterday" '+%d-%m-%Y')
-FILENAME=$PROJECT-$YESTERDAY.sql.gz
-s3cmd get "s3://scrap-db-backups/$PROJECT/$FILENAME" /tmp
+FILENAME="$PROJECT-$YESTERDAY.sql.gz"
+s3cmd get "s3://$BUCKET/$PROJECT/$FILENAME" /tmp
 gunzip -c "/tmp/$FILENAME" | mysql -u "$DB_USER" -p"$DB_PASSWORD" -h "$DB_HOST" -D "$PROJECT"
