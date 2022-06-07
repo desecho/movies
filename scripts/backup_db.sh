102467bbfaac185930123e79647a883a0dd89198
Anton Samarchyan <desecho@gmail.com>
2022-06-06 21:49:14 -0400

    Rename variable in backup db script

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -2,6 +2,6 @@
 
 set -eou pipefail
 
-NOW="$(date +"%d-%m-%Y")"
-ARCHIVE="${PROJECT}-${NOW}.sql.gz"
+TODAY="$(date +"%d-%m-%Y")"
+ARCHIVE="${PROJECT}-${TODAY}.sql.gz"
 mysqldump -u root -h "${DB_HOST}" "${PROJECT}" -p"${DB_PASSWORD}" | gzip -9 > "${ARCHIVE}"
