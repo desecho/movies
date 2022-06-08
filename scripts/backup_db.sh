46f52d1810e6e423a18d65399528d6f867591e03
Anton Samarchyan <desecho@gmail.com>
2022-06-07 20:52:56 -0400

    Standardize backup db script

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -3,5 +3,5 @@
 set -eou pipefail
 
 TODAY="$(date +"%d-%m-%Y")"
-ARCHIVE="${PROJECT}-${TODAY}.sql.gz"
-mysqldump -u root -h "${DB_HOST}" "${PROJECT}" -p"${DB_PASSWORD}" | gzip -9 > "${ARCHIVE}"
+FILENAME="${PROJECT}-${TODAY}.sql.gz"
+mysqldump -u root -h "${DB_HOST}" "${PROJECT}" -p"${DB_PASSWORD}" | gzip -9 > "${FILENAME}"
