68e777e5f0895c673a78a03816fb225e0458dc09
Anton Samarchyan <desecho@gmail.com>
2022-06-12 01:18:33 -0400

    Reorder variables in the env.sh file

--- env.sh
+++ env.sh
@@ -7,17 +7,19 @@ export COLLECT_STATIC=True
 # bool
 export IS_DEV=True
 # bool
-export IS_CELERY_DEBUG=True
+export DISABLE_CSRF=
 export SECRET_KEY=key
 export PROJECT_DOMAIN=localhost
 export INTERNAL_IP=127.0.0.1
 export STATIC_URL=/static/
-# bool
-export DISABLE_CSRF=
 export SENTRY_DSN=
 export GOOGLE_ANALYTICS_ID=id
-export DJANGO_SETTINGS_MODULE=movies.settings
-export REDIS_URL=redis://localhost:6379/
+
 export DB_USER=root
 export DB_PASSWORD=password
 export DB_HOST=127.0.0.1
+
+# bool
+export IS_CELERY_DEBUG=True
+export DJANGO_SETTINGS_MODULE=movies.settings
+export REDIS_URL=
