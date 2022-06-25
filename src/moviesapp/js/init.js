fdaa52d05cddf2f8b05a707837f7fd148cc1351e
Anton Samarchyan <desecho@gmail.com>
2022-06-25 18:48:13 -0400

    Comment out vk code. Fix js bugs

--- src/moviesapp/js/init.js
+++ src/moviesapp/js/init.js
@@ -1,41 +1,38 @@
-/* global VK:false */
-
 'use strict';
 
 import {newApp} from './app';
 
-function getIsVkApp() {
-  function inIframe() {
-    try {
-      return window.self !== window.top;
-    } catch (e) {
-      return true;
-    }
-  }
+// function getIsVkApp() {
+//   function inIframe() {
+//     try {
+//       return window.self !== window.top;
+//     } catch (e) {
+//       return true;
+//     }
+//   }
 
-  if (vars.isVkUser) {
-    return inIframe();
-  }
-  return false;
-}
+//   if (vars.isVkUser) {
+//     return inIframe();
+//   }
+//   return false;
+// }
 
 newApp({
   methods: {
     changeLanguage() {
       document.getElementById('language-form').submit();
     },
-    invite() {
-      VK.callMethod('showInviteBox');
-    },
+    // invite() {
+    //   VK.callMethod('showInviteBox');
+    // },
   },
 }).mount('#menu');
 
-const vars = window.vars;
-vars.isVkApp = getIsVkApp();
+// vars.isVkApp = getIsVkApp();
 
-if (vars.isVkApp) {
-  $('.vk-app-show').show();
-  $('#content').addClass('vk');
-} else {
-  $('.vk-app-hide').show();
-}
+// if (vars.isVkApp) {
+//   $('.vk-app-show').show();
+//   $('#content').addClass('vk');
+// } else {
+//   $('.vk-app-hide').show();
+// }
