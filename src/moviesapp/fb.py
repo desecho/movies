8ab43ac859a20a7f4f8183acf57b52203d042fb0
Anton Samarchyan <desecho@gmail.com>
2022-07-05 20:17:25 -0400

    Comment out fb related code

--- src/moviesapp/fb.py
+++ src/moviesapp/fb.py
@@ -2,7 +2,6 @@
 from typing import TYPE_CHECKING
 
 from django.contrib.auth import get_user_model
-from django.core.cache import cache
 from django.db.models import QuerySet
 from facebook import GraphAPI
 
@@ -18,15 +17,18 @@ class Fb:
         access_token = user.get_fb_account().extra_data["access_token"]
         self.fb = GraphAPI(access_token=access_token, version="2.7")
 
-    def get_friends(self) -> QuerySet["User"]:
+    @staticmethod
+    def get_friends() -> QuerySet["User"]:
         """Get friends from Facebook."""
-        fb_friends = cache.get("fb_friends")
-        if fb_friends is None:
-            fb_friends = self.fb.get_connections(id="me", connection_name="friends")["data"]
-            cache.set("fb_friends", fb_friends)
-        friends_ids = [f["id"] for f in fb_friends]
         user_model: "User" = get_user_model()  # type: ignore
-        friends: QuerySet["User"] = user_model.objects.filter(
-            social_auth__provider="facebook", social_auth__uid__in=friends_ids
-        ).exclude(hidden=True)
-        return friends
+        return user_model.objects.none()
+        # fb_friends = cache.get("fb_friends")
+        # if fb_friends is None:
+        #     fb_friends = self.fb.get_connections(id="me", connection_name="friends")["data"]
+        #     cache.set("fb_friends", fb_friends)
+        # friends_ids = [f["id"] for f in fb_friends]
+        # user_model: "User" = get_user_model()  # type: ignore
+        # friends: QuerySet["User"] = user_model.objects.filter(
+        #     social_auth__provider="facebook", social_auth__uid__in=friends_ids
+        # ).exclude(hidden=True)
+        # return friends
