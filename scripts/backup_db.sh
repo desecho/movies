92f031eeac4374405701a08c9e326c03ce95a327
Anton Samarchyan <desecho@gmail.com>
2022-07-10 23:26:13 -0400

    Fix backup db script

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -4,4 +4,4 @@ set -eou pipefail
 
 TODAY="$(date +"%d-%m-%Y")"
 FILENAME="${PROJECT}-${TODAY}.sql.gz"
-mysqldump -u "${PROJECT}" -h "${DB_HOST}" "${PROJECT}" -p"${DB_PASSWORD}" | gzip -9 > "${FILENAME}"
+mysqldump -u "${PROJECT}" -h "${DB_HOST}" "${PROJECT}" -p"${DB_PASSWORD}" --no-tablespaces | gzip -9 > "${FILENAME}"
